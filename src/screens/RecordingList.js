c646fc524184b177773f0760ee1c06991be6eb61
WilliamP <minhslider98@gmail.com>
2019-10-17 00:31:14 +1000

    Recorder App 2.0

--- src/screens/RecordingList.js
+++ src/screens/RecordingList.js
@@ -1,46 +1,82 @@
 import React from "react";
 import {
-  View,
   StyleSheet,
   Text,
-  Button,
+  View,
   FlatList,
   TouchableOpacity
 } from "react-native";
-import { sounds } from "../components/SoundForm";
+import { navigate } from "../navigationRef";
 import { ListItem } from "react-native-elements";
+import { soundArray } from "./CreateRecord";
+import { AsyncStorage } from "react-native";
 
 const BACKGROUND_COLOR = "#FFFFFF";
 
-const RecordingList = ({ navigation }) => {
-  console.log(sounds);
-  return (
-    <View style={styles.background}>
-      <Text>List Records</Text>
-      <FlatList
-        keyExtractor={item => item.name}
-        data={sounds}
-        renderItem={({ item }) => {
-          return (
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate("CurrentRecord", { name: item.name })
-              }
-            >
-              <ListItem chevron title={item.name} />
-            </TouchableOpacity>
-          );
-        }}
-      />
-    </View>
-  );
-};
+export default class RecordingList extends React.Component {
+  state = {
+    sounds: []
+  };
 
+  constructor() {
+    super();
+    this.retrievedSounds = [];
+  }
+
+  componentDidMount() {
+    // this.getSounds().then(sounds => {
+    //   this.setState({
+    //     sounds: sounds
+    //   });
+    // });
+  }
+
+  getSounds = () =>
+    new Promise(function(resolve) {
+      AsyncStorage.getItem("soundArray").then(val => {
+        let retrievedSoundArray = JSON.parse(val);
+        return resolve(retrievedSoundArray);
+      });
+    });
+
+  _retrieveData = async () => {
+    try {
+      let val = AsyncStorage.getItem("soundArray").then(result => {
+        let retrievedSoundArray = JSON.parse(val);
+        // console.log(retrievedSoundArray);
+        this.retrievedSounds = retrievedSoundArray;
+        // console.log(this.retrievedSounds);
+        return retrievedSoundArray;
+      });
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+  render() {
+    return (
+      <View style={styles.background}>
+        <Text>List Records</Text>
+        <FlatList
+          keyExtractor={item => item.id}
+          data={soundArray}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                onPress={() => navigate("CurrentRecord", { id: item.id })}
+              >
+                <ListItem chevron title={item.name} description={item.desc} />
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: BACKGROUND_COLOR
   }
 });
-
-export default RecordingList;
